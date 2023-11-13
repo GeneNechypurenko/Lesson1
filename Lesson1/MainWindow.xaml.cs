5880b350cc654b36d63835899b473fa1ed143122
Eugene Nechypurenko <e.nechypurenko@gmail.com>
2023-11-14 01:07:53 +0200

    Add project files.

---
+++ Lesson1/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace Lesson1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private decimal total;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            total += decimal.Parse(valueTextBox.Text);
+            outputLabel.Content = "total: " + total.ToString("C");
+        }
+    }
+}
---
+++ task2/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace task2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Dictionary<string, string> userAccess = new()
+        {
+            { "Admin", "123133" },
+            { "login", "password" },
+            { "Valera", "Va1er4ik" },
+            { "Kaban777", "777" },
+            { "SupaDupa", "krosav4eg" },
+        };
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void loginButton_Click(object sender, RoutedEventArgs e) => MessageBox.Show
+                (userAccess.ContainsKey(loginTextBox.Text) && userAccess.ContainsValue(passwordTextBox.Text)
+                 ? "Authentication successful"
+                 : "Access denied");
+    }
+}
